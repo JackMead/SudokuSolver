aa9100312955c21c35649f8162460eb3b6850a83
JackMead <Jack.Mead@softwire.com>
2018-01-26 15:08:59 +0000

    Working solver

--- SudokuSolver/SudokuSolver/Program.cs
+++ SudokuSolver/SudokuSolver/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -21,18 +22,104 @@ namespace SudokuSolver
             Console.WriteLine("Starting Sudoku:");
             PrintSudoku(sudoku);
 
-            while (true)
+            var nums = FindAvailableNumbers(sudoku, 0, 3);
+            foreach (var num in nums)
             {
-                UpdateAvailableOptions(sudoku);
+                Console.Write(num);
             }
 
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Console.WriteLine("Completing:");
+            CompleteSudoku(sudoku);
+            stopwatch.Stop();
+            PrintSudoku(sudoku);
+            Console.WriteLine("Program took, {0}",stopwatch.Elapsed);
         }
 
-        public void UpdateAvailableOptions(Sudoku sudoku)
+        public void CompleteSudoku(Sudoku sudoku)
         {
-            RemoveRowOptions();
-            RemoveColOptions();
-            RemoveBoxOptions();
+            for (int i = 0; i < 9; i++)
+            {
+                if (RecursivelyFillInNextNumberIfPossible(sudoku, 0, 0, i))
+                {
+                    break;
+                }
+            }
+        }
+
+        public bool RecursivelyFillInNextNumberIfPossible(Sudoku sudoku, int row, int col, int attempts)
+        {
+            //PrintSudoku(sudoku);
+
+            var startingValue = sudoku.Squares[row, col];
+            if (startingValue == 0)
+            {
+                //Find available squares for this square
+                var availableSquares = FindAvailableNumbers(sudoku, row, col);
+                if (availableSquares.Count <= attempts)
+                {
+                    return false;
+                }
+                sudoku.Squares[row, col] = availableSquares[attempts];
+            }
+            
+            var newCol = col < 8 ? col + 1 : 0;
+            var newRow = col < 8 ? row : row + 1;
+            if (newRow == 9)
+            {
+                return true;
+            }
+            var optionsNextNumber = FindAvailableNumbers(sudoku, newRow, newCol);
+
+            for (int i = 0; i < optionsNextNumber.Count; i++)
+            {
+                if (RecursivelyFillInNextNumberIfPossible(sudoku, newRow, newCol, i))
+                {
+                    return true;
+                }
+            }
+            sudoku.Squares[row, col] = startingValue;
+            return false;
+        }
+
+        public List<int> FindAvailableNumbers(Sudoku sudoku, int thisRow, int thisCol)
+        {
+            if (sudoku.Squares[thisRow, thisCol]!=0)
+            {
+                return new List<int>{sudoku.Squares[thisRow, thisCol]};
+            }
+            var availableNumbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //Remove row duplicates
+            for (int col = 0; col < 9; col++)
+            {
+                if (sudoku.Squares[thisRow, col] != 0)
+                {
+                    availableNumbers.Remove(sudoku.Squares[thisRow, col]);
+                }
+            }
+            //Remove col duplicates
+            for (int row = 0; row < 9; row++)
+            {
+                if (sudoku.Squares[row, thisCol] != 0)
+                {
+                    availableNumbers.Remove(sudoku.Squares[row, thisCol]);
+                }
+            }
+            //Remove box duplicates
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    var rowToCheck = 3*(thisRow / 3) + row;
+                    var colToCheck = 3*(thisCol / 3) + col;
+                    if (sudoku.Squares[rowToCheck, colToCheck] != 0)
+                    {
+                        availableNumbers.Remove(sudoku.Squares[rowToCheck, colToCheck]);
+                    }
+                }
+            }
+            return availableNumbers;
         }
 
         public void PrintSudoku(Sudoku sudoku)
@@ -41,45 +128,29 @@ namespace SudokuSolver
             {
                 for (int col = 0; col < 9; col++)
                 {
-                    if (sudoku.Squares[row, col].Count == 1)
-                    {
-                        Console.Write(" {0} ", sudoku.Squares[row, col][0]);
-                    }
-                    else
-                    {
-                        Console.Write(" 0 ");
-                    }
+                    Console.Write(" {0} ", sudoku.Squares[row, col]);
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine();
         }
 
         public Sudoku LoadSudoku()
         {
             var path = "sudokuStartingVals.csv";
-            var squares = new List<int>[9,9];
+            var squares = new int[9, 9];
             using (var sr = new StreamReader(path))
             {
                 var line = "";
                 var lineNum = 0;
                 while ((line = sr.ReadLine()) != null)
                 {
-                     var ints = line.Split(',');
+                    var ints = line.Split(',');
                     for (int i = 0; i < 9; i++)
                     {
-                        squares[lineNum,i]=new List<int>{int.Parse(ints[i])};
+                        squares[lineNum, i] = int.Parse(ints[i]);
                     }
                     lineNum++;
-                }
-            }
-            for (int row = 0; row < 9; row++)
-            {
-                for (int col = 0; col < 9; col++)
-                {
-                    if (squares[row, col].Contains(0))
-                    {
-                        squares[row, col] = new List<int> {1, 2, 3, 4, 5, 6, 7, 8, 9};
-                    }
                 }
             }
             return new Sudoku(squares);
@@ -88,9 +159,9 @@ namespace SudokuSolver
 
     public class Sudoku
     {
-        public List<int>[,] Squares { get; set; }
+        public int[,] Squares { get; set; }
 
-        public Sudoku(List<int>[,] squares)
+        public Sudoku(int[,] squares)
         {
             Squares = squares;
         }
